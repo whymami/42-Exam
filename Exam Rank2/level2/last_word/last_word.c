86a292a504358ccc62688e24aaef600aca80c364
muhammed maruf güveli <98236934+whymami@users.noreply.github.com>
2024-03-03 07:42:01 -0500

    whymami
    -last_word

---
+++ Exam Rank2/level2/last_word/last_word.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+void	last_word(char *str)
+{
+	int	j = -1;
+	int i = 0;
+
+	while (str[++i])
+		if ((str[i] == ' ' || str[i] == '\t') && (str[i + 1] >= 33 && str[i + 1] <= 126))
+			j = i + 1;
+	while (str[j] >= 33 && str[j] <= 127)
+		write(1, &str[j++], 1);
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc == 2)
+		last_word(argv[1]);
+	write(1, "\n", 1);
+	return (0);
+}
